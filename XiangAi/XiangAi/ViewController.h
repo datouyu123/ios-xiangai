d386b0d9bb73585d8da15fe3aaf9e882e53d04be
Yao <391426555@qq.com>
2016-01-16 18:00:49 +0800

    测试
    测试

---
+++ XiangAi/XiangAi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XiangAi
+//
+//  Created by 张伟颖 on 16/1/16.
+//  Copyright © 2016年 xdrj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XiangAi/XiangAi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XiangAi
+//
+//  Created by 张伟颖 on 16/1/16.
+//  Copyright © 2016年 xdrj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
